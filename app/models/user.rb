04c95a4a452128f2307dac9b1356a0fea3c74fa4
raghav <128453971+raghavharikesh@users.noreply.github.com>
2026-05-06 12:30:22 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
-	
-	validates :username, presence:true#,uniqueness: true
-	validates :password,presence:true#,uniqueness: true
-	has_secure_password
-	# with_options if: :username? do |username|
- #    username.validates :username ,uniqueness: true,length: { minimum: 5 }
- #    username.validates :password, presence: true
- end
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+  validates :name, presence: true
+
+  # JWT Methods
+  def generate_jwt
+    JsonWebToken.encode(user_id: id)
+  end
+
+  private
+
+  def password_required?
+    new_record? || password.present?
+  end
+end
